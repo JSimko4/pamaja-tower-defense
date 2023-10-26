829b5805641cfd62b2c2afde80c44a355eac397c
sacredmasiv <sacredmasiv@gmail.com>
2023-10-26 16:12:22 +0200

    map creating with file

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -14,14 +14,23 @@ public class LevelManager : MonoBehaviour
     private Camera localCamera;
 
 
-    private float tileSize;
+
+ 
 
     private Vector3 worldStart;
 
+    public GameObject roadTile; 
+    public GameObject otherTile; 
+
+    private string mapFileName = "Level1";
+    private float tileSize ;
     // Start is called before the first frame update
     void Start()
     {
+        
+
         tileSize = tile1.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        LoadMap();
         worldStart = localCamera.ScreenToWorldPoint(new Vector3(0, Screen.height));
 
 
@@ -31,6 +40,33 @@ public class LevelManager : MonoBehaviour
         first.z = 0;
 
         Instantiate(tile1, first, Quaternion.identity);
+    }
+
+
+
+
+    void LoadMap()
+    {
+        TextAsset mapText = Resources.Load<TextAsset>(mapFileName);
+        string[] mapLines = mapText.text.Split('\n');
+
+        for (int i = 0; i < mapLines.Length; i++)
+        {
+            for (int j = 0; j < mapLines[i].Length; j++)
+            {
+                char tileChar = mapLines[i][j];
+                Vector2 spawnPosition = new Vector2(j * tileSize, -i * tileSize); // Negative on 'i' to start from top
+
+                if (tileChar == '1')
+                {
+                    Instantiate(tile1, spawnPosition, Quaternion.identity);
+                }
+                else if (tileChar == '0')
+                {
+                    Instantiate(tile2, spawnPosition, Quaternion.identity);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
