98b4c86b7a886258d0b6ed446d55a8d430a877df
Patrik Sabol <psabol571@gmail.com>
2023-10-27 16:18:49 +0200

    Added singleton for GameManager, LevelManager, one button for towert that doesn't do anything yet

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : Singleton<LevelManager>
 {
     [SerializeField]
     private GameObject tile1;
@@ -23,6 +23,8 @@ public class LevelManager : MonoBehaviour
 
     private string mapFileName = "Level1";
     private float tileSize ;
+
+
 
     // Start is called before the first frame update
     void Start()
