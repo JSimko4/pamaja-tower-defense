792c14b0947c1e5d35cd2338391380eebebbdf5c
Patrik Sabol <psabol571@gmail.com>
2023-12-02 22:33:37 +0100

    Freeze and Heal skill prepared

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -8,6 +8,8 @@ public class Explosion : MonoBehaviour
     private int heal;
     private float stunDuration;
 
+    private bool initialized = false;
+
     public void Init(Vector3 position, float radius ,int damage=0, float stunDuration =0, int heal =0,  float disappearAfter = 0.3f)
     {
         transform.position = position;
@@ -17,16 +19,24 @@ public class Explosion : MonoBehaviour
         this.stunDuration = stunDuration;
         this.heal = heal;
 
+        initialized = true;
+
+        Debug.Log("Explosion INIT");
+
         Invoke("Destruct", disappearAfter);
     }
 
     private void Destruct()
     {
+        Debug.Log("Explosion destroy");
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!initialized)
+            return;
+
         if(other.tag == "Monster")
         {
             var monster = other.GetComponent<Monster>();
---
+++ Assets/Scripts/HealSkill.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealSkill : Skill
+{
+    [SerializeField]
+    private int healAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Explosion>().Init(transform.position, Radius, 0, 0, healAmount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
