64fd38fa61a9a424101aee02f67a3be2f71effb1
JSimko4 <xsimkoj2@stuba.sk>
2023-12-03 16:35:19 +0100

    add jsonproperty to LevelData class

--- Assets/Scripts/JSON/LevelData.cs
+++ Assets/Scripts/JSON/LevelData.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 [System.Serializable]
 public class LevelData
 {
+    [JsonProperty("grid")]
     public List<string> grid;
 
     [JsonProperty("startTilePositions")]
